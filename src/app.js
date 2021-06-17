efd8299e2431111e5171dd230228a82cea404804
claudiobelempb <claudio.c.lima@hotmail.com>
2021-06-17 17:54:05 -0300

    Iniciando o Projeto (2/2)

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const router = express.Router();
+
+router.get('/', (request, response) => {
+  return response.json({
+    message: "Hello World!",
+  });
+});
+app.use('/', router);
+
+export { app };
--- src/server.js
+++ src/server.js
@@ -1,1 +1,6 @@
-import express from 'express';
+import { app } from './app';
+
+app.set('port', 3000);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Server run in port: ${server.address().port}`);
+});
