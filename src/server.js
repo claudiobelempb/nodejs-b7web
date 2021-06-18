baf8335d4d6252f247be5ebdf3c2b67c95812008
claudiobelempb <claudio.c.lima@hotmail.com>
2021-06-18 10:13:34 -0300

    Configurando o MongoDB

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import { app } from './app';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
 dotenv.config({path: "variables.env"});
+mongoose.connect(process.env.DATABASE, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (error) => {
+  console.error(`Error: ${error.message}`);
+});
 
 app.set('port', process.env.PORT || 3000);
 const server = app.listen(app.get('port'), () => {
